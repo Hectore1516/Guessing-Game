1657cea0536fc54aa86aafd3912aeabc753b5dac
unknown <hectore1@outlook.es>
2022-04-24 22:47:20 -0500

    new version

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,17 +1,17 @@
 function guessloop {
-  while [[ $response -ne $correct_files ]]
-  do 
-    if [[ $response -lt $correct_files ]]
-    then
-      echo "There's more files than your guess. Enter a number again:"
-      read response
-    elif [[ $response -gt correct_files ]]
-    then
-      echo "There's less files than your guess. Enter a number again:"
-      read response
-    fi
-  done
-  echo "You got the correct answer!"
+    while [[ $response -ne $correct_files ]]
+    do 
+        if [[ $response -lt $correct_files ]]
+        then
+            echo "There's more files than your guess. Enter a number again:"
+            read response
+        elif [[ $response -gt correct_files ]]
+        then
+            echo "There's less files than your guess. Enter a number again:"
+            read response
+        fi
+    done
+        echo "You got the correct answer!"
 }
 
 echo "Plese enter how many files are in the current directory:"
